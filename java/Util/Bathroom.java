5e307ae88f478e49755e1bcc82fbb109c84f3fd8
Lex Yu <lsyu@usc.edu>
2022-04-30 17:38:21 -0700

    add image url to bathroom

--- java/Util/Bathroom.java
+++ java/Util/Bathroom.java
@@ -6,6 +6,7 @@ public class Bathroom {
 	//bathroom
 	private String bathroom_id;
 	private String bathroom_location;
+	private String image_url;
 	
 	public String getBathroomId() {
 		return bathroom_id;
@@ -15,12 +16,20 @@ public class Bathroom {
 		return bathroom_location;
 	}
 	
+	public String getImageUrl() {
+		return image_url;
+	}
+	
 	public String setBathroomId(String id) {
 		bathroom_id = id;
 	}
 	
 	public String setBathroomLocation(String loc) {
 		bathroom_location = loc;
+	}
+	
+	public String setImageUrl(String url) {
+		image_url = url;
 	}
 
 }
